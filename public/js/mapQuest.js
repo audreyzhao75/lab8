6a7fb53ed00fd8a7bac4fff761c9695b49a747a3
Audrey Zhao <a2zhao@ucsd.edu>
2021-02-25 12:57:12 -0600

    finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+    L.mapquest.key = 'OBVONxYIcTFBkXafJeB9r8E0AKh45f0X'
+
+    var map = L.mapquest.map('map', {
+        center: [32.88, -117.236],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoomControl: false
+    });
+
+    L.marker([32.879, -117.237]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,12 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response) {
+    var rsvpEmail = request.body.rsvpEmail;
+    //print when POST request made
+    console.log(rsvpEmail);
+    data.rsvp.push(rsvpEmail);
+    //send back data
+    response.send(rsvpEmail);
+}
